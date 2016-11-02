b188b5893e1e0ee14e05dd150999a263337b3104
xavierouicar <xavier@ouicar.fr>
2016-11-02 18:30:33 +0100

    fix warning deprecations

--- app/views/cars/_car.json.jbuilder
+++ app/views/cars/_car.json.jbuilder
@@ -1,2 +1,1 @@
 json.extract! car, :id, :registration, :user_id, :model, :brand, :created_at, :updated_at
-json.url car_url(car, format: :json)
--- spec/controllers/cars_controller_spec.rb
+++ spec/controllers/cars_controller_spec.rb
@@ -28,7 +28,8 @@ RSpec.describe CarsController, type: :controller do
   describe "GET #index" do
     it "assigns all cars as @cars", :show_in_doc do
       car = create(:car)
-      get :index, user_id: car.user.id, format: :json
+      auth_headers = car.user.create_new_auth_token
+      get :index, params: {user_id: car.user.id, format: :json}, headers: auth_headers
       expect(assigns(:cars)).to eq([car])
     end
   end
@@ -36,7 +37,8 @@ RSpec.describe CarsController, type: :controller do
   describe "GET #show" do
     it "assigns the requested car as @car", :show_in_doc do
       car = create(:car)
-      get :show, user_id: car.user.id, id: car.to_param, format: :json
+      auth_headers = car.user.create_new_auth_token
+      get :show, params: {user_id: car.user.id, id: car.to_param, format: :json}, headers: auth_headers
       expect(assigns(:car)).to eq(car)
     end
   end
@@ -45,21 +47,24 @@ RSpec.describe CarsController, type: :controller do
     context "with valid params" do
       it "creates a new Car", :show_in_doc do
         user = create(:user)
+        auth_headers = user.create_new_auth_token
         expect {
-          post :create, user_id: user.id, car: valid_attributes, format: :json
+          post :create, params: {user_id: user.id, car: valid_attributes, format: :json}, headers: auth_headers
         }.to change(Car, :count).by(1)
       end
 
       it "assigns a newly created car as @car" do
         user = create(:user)
-        post :create, user_id: user.id, car: valid_attributes, format: :json
+        auth_headers = user.create_new_auth_token
+        post :create, params: {user_id: user.id, car: valid_attributes, format: :json}, headers: auth_headers
         expect(assigns(:car)).to be_a(Car)
         expect(assigns(:car)).to be_persisted
       end
 
       it "response status created" do
         user = create(:user)
-        post :create, user_id: user.id, car: valid_attributes, format: :json
+        auth_headers = user.create_new_auth_token
+        post :create, params: {user_id: user.id, car: valid_attributes, format: :json}, headers: auth_headers
         expect(response.status).to eq(201)
       end
     end
@@ -67,13 +72,15 @@ RSpec.describe CarsController, type: :controller do
     context "with invalid params" do
       it "assigns a newly created but unsaved car as @car" do
         user = create(:user)
-        post :create, user_id: user.id, car: invalid_attributes, format: :json
+        auth_headers = user.create_new_auth_token
+        post :create, params: {user_id: user.id, car: invalid_attributes, format: :json}, headers: auth_headers
         expect(assigns(:car)).to be_a_new(Car)
       end
 
       it "response status unprocessable entity", :show_in_doc do
         user = create(:user)
-        post :create, user_id: user.id, car: invalid_attributes, format: :json
+        auth_headers = user.create_new_auth_token
+        post :create, params: {user_id: user.id, car: invalid_attributes, format: :json}, headers: auth_headers
         expect(response.status).to eq(422)
       end
     end
@@ -85,22 +92,25 @@ RSpec.describe CarsController, type: :controller do
         skip("Add a hash of attributes valid for your model")
       }
 
-      it "updates the requested car", :show_in_doc do
+      it "updates the requested car" do
         car = create(:car)
-        put :update, user_id: car.user.id, id: car.to_param, car: new_attributes, format: :json
+        auth_headers = car.user.create_new_auth_token
+        put :update, params: {user_id: car.user.id, id: car.to_param, car: new_attributes, format: :json}, headers: auth_headers
         car.reload
         skip("Add assertions for updated state")
       end
 
       it "assigns the requested car as @car" do
         car = create(:car)
-        put :update, user_id: car.user.id, id: car.to_param, car: valid_attributes, format: :json
+        auth_headers = car.user.create_new_auth_token
+        put :update, params: {user_id: car.user.id, id: car.to_param, car: valid_attributes, format: :json}, headers: auth_headers
         expect(assigns(:car)).to eq(car)
       end
 
-      it "status ok" do
+      it "status ok", :show_in_doc, title: 'success' do
         car = create(:car)
-        put :update, user_id: car.user.id, id: car.to_param, car: valid_attributes, format: :json
+        auth_headers = car.user.create_new_auth_token
+        put :update, params: {user_id: car.user.id, id: car.to_param, car: valid_attributes, format: :json}, headers: auth_headers
         expect(response.status).to eq(200)
       end
     end
@@ -108,13 +118,15 @@ RSpec.describe CarsController, type: :controller do
     context "with invalid params" do
       it "assigns the car as @car" do
         car = create(:car)
-        put :update, user_id: car.user.id, id: car.to_param, car: invalid_attributes , format: :json
+        auth_headers = car.user.create_new_auth_token
+        put :update, params: {user_id: car.user.id, id: car.to_param, car: invalid_attributes , format: :json}, headers: auth_headers
         expect(assigns(:car)).to eq(car)
       end
 
-      it "response status unprocessable entity", :show_in_doc do
+      it "response status unprocessable entity", :show_in_doc, title: 'error' do
         car = create(:car)
-        put :update, user_id: car.user.id, id: car.to_param, car: invalid_attributes, format: :json
+        auth_headers = car.user.create_new_auth_token
+        put :update, params: {user_id: car.user.id, id: car.to_param, car: invalid_attributes, format: :json}, headers: auth_headers
         expect(response.status).to eq(422)
       end
     end
@@ -123,14 +135,16 @@ RSpec.describe CarsController, type: :controller do
   describe "DELETE #destroy" do
     it "destroys the requested car", :show_in_doc do
       car = create(:car)
+      auth_headers = car.user.create_new_auth_token
       expect {
-        delete :destroy, user_id: car.user.id, id: car.to_param, format: :json
+        delete :destroy, params: {user_id: car.user.id, id: car.to_param, format: :json}, headers: auth_headers
       }.to change(Car, :count).by(-1)
     end
 
     it "response status no_content" do
       car = create(:car)
-      delete :destroy, user_id: car.user.id, id: car.to_param, format: :json
+      auth_headers = car.user.create_new_auth_token
+      delete :destroy, params: {user_id: car.user.id, id: car.to_param, format: :json}, headers: auth_headers
       expect(response.status).to eq(204)
     end
   end
