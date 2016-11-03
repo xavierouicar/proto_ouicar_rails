6f762c65e54f9615e5d4a78fecbc32c2c278323d
xavierouicar <xavier@ouicar.fr>
2016-11-03 15:28:50 +0100

    set unprocessable_entity for apipie validations

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,13 +1,19 @@
 class ApiController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  # ParamError is superclass of ParamMissing, ParamInvalid
+  rescue_from Apipie::ParamError do |e|
+    render text: e.message, status: :unprocessable_entity
+  end
+
   def user_verification
     user = User.find(params[:user_id])
     if current_user
       if current_user != user
-        render status: :forbidden, text: 'Invalid User connected'
+        render status: :unauthorized, text: 'Invalid User connected'
       end
     else
-      render status: :forbidden, text: 'User not connected'
+      render status: :unauthorized, text: 'User not connected'
     end
   end
 end
